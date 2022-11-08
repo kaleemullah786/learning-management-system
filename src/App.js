afea0b2d9cb8f33588c88a608397f890d7650b38
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-11-08 15:19:49 +0300

    register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/Login'
+import Register from './components/SignUp/Register';
 function App() {
   return (
     <div className="App">
     <Login />
+    {/* <Register /> */}
     </div>
   );
 }
---
+++ src/components/SignUp/Register.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Form, Input, Checkbox, Button} from 'antd';
+import 'antd/dist/antd.css';
+import './index.css'
+
+const Register = () => {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="register-page">
+      <div className="register-box">
+        <div className="illustration-wrapper">
+          <img src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700" alt="register"/>
+        </div>
+        <Form
+          name="register-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <p className="form-title">Create account</p>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please input your emaild!' }]}
+          >
+            <Input
+              type="email"
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input
+              placeholder="Username"
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password 
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="register-form-button">
+              SIGN UP
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
+
